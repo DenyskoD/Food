e5bbb65db307cbc0220009ec933d97dca4b5d1c6
DenyskoD <deniskodima1@gmail.com>
2020-08-12 18:42:18 +0300

    classes for cards by JS

--- docs/js/script.js
+++ docs/js/script.js
@@ -128,7 +128,89 @@ window.addEventListener("DOMContentLoaded", () => {
         }
       });
     }
+  }); // modal
+
+  let modal = document.querySelector(".modal");
+  let modalShowBtns = document.querySelectorAll("[data-modal]");
+  let modalClose = document.querySelector("[data-close]");
+
+  function modalShow() {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    document.body.style.overflow = "hidden";
+    clearInterval(modalTimerId);
+  }
+
+  modalShowBtns.forEach(item => {
+    item.addEventListener('click', modalShow);
   });
+
+  function modalHide() {
+    modal.classList.remove("show");
+    modal.classList.add("hide");
+    document.body.style.overflow = "";
+  }
+
+  modalClose.addEventListener('click', modalHide);
+  modal.addEventListener('click', e => {
+    if (e.target == modal) {
+      modalHide();
+    }
+  });
+  document.addEventListener('keydown', e => {
+    if (e.code === "Escape") {
+      modalHide();
+    }
+  });
+  let modalTimerId = setTimeout(modalShow, 60000);
+
+  function showModalByScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+      modalShow();
+      window.removeEventListener('scroll', showModalByScroll);
+    }
+  }
+
+  window.addEventListener('scroll', showModalByScroll); // class for card
+
+  class MenuCard {
+    constructor(src, alt, title, desc, price, parentSelector, ...classes) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.desc = desc;
+      this.price = price;
+      this.classes = classes;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 25;
+      this.changeToUAH();
+    }
+
+    changeToUAH() {
+      this.price *= this.transfer;
+    }
+
+    render() {
+      const element = document.createElement('div');
+      this.classes.forEach(className => element.classList.add(className));
+      element.innerHTML += `
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.desc}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            `;
+      this.parent.append(element);
+    }
+
+  }
+
+  new MenuCard("img/tabs/vegy.jpg", "vegy", 'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 9, ".menu .container", 'menu__item').render();
+  new MenuCard("img/tabs/elite.jpg", "elite", 'Меню "Премиум"', 'Меню "Премиум" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 11, ".menu .container", 'menu__item').render();
+  new MenuCard("img/tabs/post.jpg", "post", 'Меню "Постное"', 'Меню "Постное" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 13, ".menu .container", 'menu__item').render();
 });
 
 /***/ })
